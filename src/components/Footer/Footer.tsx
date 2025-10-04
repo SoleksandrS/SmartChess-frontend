575abf5bec3a2f50c81e0cad3b0cb1e16a5d256d
SOleksandrS <o.y.sitaylo@student.khai.edu>
2025-10-04 15:39:27 +0300

    Feat: add content to Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,16 @@
+import styles from './Footer.module.scss';
+
 export function Footer() {
-  return <div>Footer</div>;
+  const email = 'contact@smartchess.ai';
+
+  return (
+    <footer className={styles['footer']}>
+      <p className={styles['copyright']}>
+        © {new Date().getFullYear()} SmartChess. All rights reserved.
+      </p>
+      <a href={`mailto:${email}`} className={styles['contact-email']}>
+        {email}
+      </a>
+    </footer>
+  );
 }
