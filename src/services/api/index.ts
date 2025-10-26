0822bdbd50ed3fa0113c71b775f45a2a4cf5a52b
SOleksandrS <o.y.sitaylo@student.khai.edu>
2025-10-26 17:30:08 +0200

    Fix: rebuild api service

---
+++ src/services/api/index.ts
@@ -0,0 +1,4 @@
+import { ENDPOINTS } from './endpoints';
+import { instance } from './instance';
+
+export { instance as api, ENDPOINTS };
--- src/services/api/instance.ts
+++ src/services/api/instance.ts
@@ -1,14 +1,8 @@
-import { envs } from 'config';
+import { envs } from '../../config';
 import axios from 'axios';
 import { STORAGE_KEYS } from 'constants/localStorage';
 
-export const API = {
-  AUTH: '/auth',
-  USERS: '/users',
-  GAMES: '/games'
-};
-
-export const api = axios.create({
+export const instance = axios.create({
   baseURL: envs.apiUrl,
   responseType: 'json',
   headers: {
@@ -16,7 +10,7 @@ export const api = axios.create({
   }
 });
 
-api.interceptors.request.use((request) => {
+instance.interceptors.request.use((request) => {
   request.headers.Authorization = `Bearer ${localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN)}`;
   return request;
 });
