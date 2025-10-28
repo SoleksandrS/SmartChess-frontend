f1f7641934bd3587b194bb090b9c6d1ca85670c5
SOleksandrS <o.y.sitaylo@student.khai.edu>
2025-10-28 20:16:40 +0200

    Fix: move getUserDataThunk from MainLayout component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import type { AppDispatch } from 'store';
+import { getUserDataThunk } from 'store/modules/auth/auth.thunk';
 import Router from './routers/Router';
 
 function App() {
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    void dispatch(getUserDataThunk());
+  }, [dispatch]);
+
   return <Router />;
 }
 
