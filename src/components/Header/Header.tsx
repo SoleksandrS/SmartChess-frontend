19af1bffa2d6bae158614c8cf9cd7d033c4b5ad2
SOleksandrS <o.y.sitaylo@student.khai.edu>
2025-10-28 21:12:46 +0200

    Feat: provide userData to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import type { TState } from 'store';
 import { ROUTES } from 'constants/routes';
 
 import { FaUserCircle } from 'react-icons/fa';
@@ -7,7 +8,7 @@ import { FaUserCircle } from 'react-icons/fa';
 import styles from './Header.module.scss';
 
 export function Header() {
-  const [isAuth] = useState(false);
+  const userData = useSelector((state: TState) => state.auth.data);
 
   return (
     <header className={styles['header']}>
@@ -18,13 +19,16 @@ export function Header() {
         </NavLink>
 
         <div className={styles['user-section']}>
-          {!isAuth ? (
+          {!userData ? (
             <NavLink to={ROUTES.SIGNIN} className={styles['auth-button']}>
               Sign In
             </NavLink>
           ) : (
-            <div className={styles['profile-icon']}>
-              <FaUserCircle size={28} />
+            <div className={styles['user-data']}>
+              <div className={styles['profile-icon']}>
+                <FaUserCircle size={28} />
+              </div>
+              <span className={styles['username']}>{userData.username}</span>
             </div>
           )}
         </div>
