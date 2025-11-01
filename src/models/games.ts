3c3529b103c8cbc2b5e59c0ccc54e1aefeea7159
SOleksandrS <o.y.sitaylo@student.khai.edu>
2025-11-01 15:05:10 +0200

    Refactor: split models file

---
+++ src/models/games.ts
@@ -0,0 +1,32 @@
+export enum EChessSide {
+  WHITE = 'w',
+  BLACK = 'b'
+}
+
+export enum EChessResult {
+  DRAW = 'draw',
+  CHECKMATE = 'checkmate'
+}
+
+export interface IGame {
+  id: string;
+  fen: string;
+  turn: EChessSide;
+  result: EChessResult | null;
+  whitePlayerId?: number;
+  blackPlayerId?: number;
+  whitePlayer?: { username: string };
+  blackPlayer?: { username: string };
+  moves: {
+    moveNumber: number;
+    turn: EChessSide;
+    move: string;
+  }[];
+}
+
+export enum EGameStatus {
+  WIN = 'win',
+  LOSE = 'lose',
+  DRAW = 'draw',
+  PLAYING = 'playing'
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -5,41 +5,5 @@ export interface IStoreAction {
   payload?: any;
 }
 
-export interface ICurrentUser {
-  id: number;
-  username: string;
-  email: string;
-}
-
-export enum EChessSide {
-  WHITE = 'w',
-  BLACK = 'b'
-}
-
-export enum EChessResult {
-  DRAW = 'draw',
-  CHECKMATE = 'checkmate'
-}
-
-export interface IGame {
-  id: string;
-  fen: string;
-  turn: EChessSide;
-  result: EChessResult | null;
-  whitePlayerId?: number;
-  blackPlayerId?: number;
-  whitePlayer?: { username: string };
-  blackPlayer?: { username: string };
-  moves: {
-    moveNumber: number;
-    turn: EChessSide;
-    move: string;
-  }[];
-}
-
-export enum EGameStatus {
-  WIN = 'win',
-  LOSE = 'lose',
-  DRAW = 'draw',
-  PLAYING = 'playing'
-}
+export * from './users';
+export * from './games';
