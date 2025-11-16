23cbbf5e75a6fe338b7a1b294be88bee8004ab41
SOleksandrS <o.y.sitaylo@student.khai.edu>
2025-11-16 17:02:25 +0200

    Feat: add HeroBanner component (add navigation to games page)

--- src/screens/Home/components/HeroBanner/HeroBanner.tsx
+++ src/screens/Home/components/HeroBanner/HeroBanner.tsx
@@ -1,3 +1,7 @@
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import type { TState } from 'store';
+import { ROUTES } from 'constants/routes';
 import { Button } from 'components';
 
 import styles from './HeroBanner.module.scss';
@@ -7,6 +11,14 @@ interface IProps {
 }
 
 export function HeroBanner({ onModalOpen }: IProps) {
+  const userData = useSelector((state: TState) => state.auth.data);
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    const path = userData ? ROUTES.GAMES : ROUTES.SIGNIN;
+    void navigate(path);
+  };
+
   return (
     <section className={styles['hero-banner']}>
       <h1 className={styles['title']}>Play & Learn Chess with AI</h1>
@@ -14,7 +26,9 @@ export function HeroBanner({ onModalOpen }: IProps) {
         Improve your skills, challenge AI, and climb the leaderboard
       </p>
       <div className={styles['buttons']}>
-        <Button variant="secondary">Play Now</Button>
+        <Button variant="secondary" onClick={onClickHandler}>
+          Play Now
+        </Button>
         <Button variant="transparent" onClick={onModalOpen}>
           Learn Chess
         </Button>
